edc74c3e3c50ec6bdc4ab9d20a20bd2162a37b73
Dylan <986641202@qq.com>
2024-12-04 21:08:12 +0800

    remove build file

--- build/third-party/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/Workspace/CPlus/third-party/googlemock
-# Build directory: /opt/Workspace/CPlus/build/third-party/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/third-party/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/Workspace/CPlus/third-party/googletest
-# Build directory: /opt/Workspace/CPlus/build/third-party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
